0d00022c1c94a1319eed40981168916ebed13698
Joshua Granick <jgranick@users.noreply.github.com>
2015-03-14 23:50:52 -0700

    Update TextRendering sample

--- TextRendering/Source/Main.hx
+++ TextRendering/Source/Main.hx
@@ -2,227 +2,50 @@ package;
 
 
 import lime.app.Application;
+import lime.graphics.opengl.GLBuffer;
+import lime.graphics.opengl.GLProgram;
+import lime.graphics.opengl.GLTexture;
+import lime.graphics.opengl.GLUniformLocation;
+import lime.graphics.Image;
+import lime.graphics.RenderContext;
 import lime.math.Matrix4;
-import lime.graphics.*;
-import lime.graphics.opengl.*;
-import lime.text.*;
-import lime.utils.*;
-
-
-class TextField {
-	
-	
-	private var vertexBuffer:GLBuffer;
-	private var indexBuffer:GLBuffer;
-	private var texture:GLTexture;
-	private var numTriangles:Int;
-	private var size:Int;
-	private var text:String;
-	private var images:Map<Int, Image>;
-	private var points:Array<TextEngine.PosInfo>;
-	
-	
-	public function new (text:String, size:Int, textFormat:TextEngine, font:Font, context:RenderContext, x:Float=0, y:Float=0) {
-		
-		points = textFormat.layout (font, size, text);
-		var glyphs = font.getGlyphs (text);
-		images = font.renderGlyphs (glyphs, size);
-		
-		this.text = text;
-		this.size = size;
-		
-		init (context, textFormat, font, x, y);
-		
-	}
-	
-	
-	public function init (context:RenderContext, textFormat:TextEngine, font:Font, x:Float, y:Float) {
-		
-		switch (context) {
-			
-			// case CANVAS (context):
-			
-			// 	context.fillStyle = "#" + StringTools.hex (config.background, 6);
-			// 	context.fillRect (0, 0, window.width, window.height);
-			// 	context.drawImage (image.src, 0, 0, image.width, image.height);
-			
-			// case DOM (element):
-			
-			// 	#if js
-			// 	element.style.backgroundColor = "#" + StringTools.hex (config.background, 6);
-			// 	element.appendChild (image);
-			// 	#end
-			
-			case FLASH (sprite):
-				
-				#if flash
-				var bitmap = new flash.display.Bitmap (image.src);
-				sprite.addChild (bitmap);
-				#end
-			
-			case OPENGL (gl):
-				
-				var vertices = new Array<Float>();
-				var indices = new Array<Int>();
-				var left, top, right, bottom;
-				
-				if (textFormat.direction == RIGHT_TO_LEFT) {
-					
-					var width = 0.0;
-					
-					for (p in points) {
-						
-						//if (!images.exists(p.codepoint)) continue;
-						//var image = images.get(p.codepoint);
-						
-						width += p.advance.x;
-						
-					}
-					
-					x -= width;
-					
-				}
-				
-				var buffer = null;
-				
-				for (p in points) {
-					
-					if (images.exists(p.codepoint)) {
-						
-						var image = images.get(p.codepoint);
-						
-						buffer = image.buffer;
-						
-						left   = image.offsetX / buffer.width;
-						top    = image.offsetY / buffer.height;
-						right  = left + image.width / buffer.width;
-						bottom = top + image.height / buffer.height;
-						
-						var pointLeft = x + p.offset.x + image.x;
-						var pointTop = y + p.offset.y - image.y;
-						var pointRight = pointLeft + image.width;
-						var pointBottom = pointTop + image.height;
-						
-						vertices.push(pointRight);
-						vertices.push(pointBottom);
-						vertices.push(right);
-						vertices.push(bottom);
-						
-						vertices.push(pointLeft);
-						vertices.push(pointBottom);
-						vertices.push(left);
-						vertices.push(bottom);
-						
-						vertices.push(pointRight);
-						vertices.push(pointTop);
-						vertices.push(right);
-						vertices.push(top);
-						
-						vertices.push(pointLeft);
-						vertices.push(pointTop);
-						vertices.push(left);
-						vertices.push(top);
-						
-						var i = Std.int(indices.length / 6) * 4;
-						indices.push(i);
-						indices.push(i+1);
-						indices.push(i+2);
-						indices.push(i+1);
-						indices.push(i+2);
-						indices.push(i + 3);
-						
-					}
-					
-					x += p.advance.x;
-					y -= p.advance.y; // flip because of y-axis direction
-				}
-				numTriangles = indices.length;
-				
-				vertexBuffer = gl.createBuffer ();
-				gl.bindBuffer (gl.ARRAY_BUFFER, vertexBuffer);
-				gl.bufferData (gl.ARRAY_BUFFER, new Float32Array (cast vertices), gl.STATIC_DRAW);
-				
-				indexBuffer = gl.createBuffer ();
-				gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-				gl.bufferData (gl.ELEMENT_ARRAY_BUFFER, new UInt8Array (cast indices), gl.STATIC_DRAW);
-				
-				var format = buffer.bitsPerPixel == 1 ? GL.ALPHA : GL.RGBA;
-				texture = gl.createTexture ();
-				gl.bindTexture (gl.TEXTURE_2D, texture);
-				#if js
-				gl.texImage2D (gl.TEXTURE_2D, 0, format, format, gl.UNSIGNED_BYTE, buffer.src);
-				#else
-				gl.texImage2D (gl.TEXTURE_2D, 0, format, buffer.width, buffer.height, 0, format, gl.UNSIGNED_BYTE, buffer.data);
-				#end
-				gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-				gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-				
-			default:
-				
-		}
-		
-	}
-	
-	
-	public function render (context:RenderContext, vertexAttribute:Int, textureAttribute:Int) {
-		
-		switch (context) {
-			
-			case OPENGL (gl):
-				
-				gl.activeTexture (gl.TEXTURE0);
-				gl.bindTexture (gl.TEXTURE_2D, texture);
-				
-				gl.bindBuffer (gl.ARRAY_BUFFER, vertexBuffer);
-				gl.vertexAttribPointer (vertexAttribute, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 0);
-				gl.vertexAttribPointer (textureAttribute, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
-				
-				gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-				gl.drawElements (gl.TRIANGLES, numTriangles, gl.UNSIGNED_BYTE, 0);
-				
-			default:
-				
-		}
-		
-	}
-	
-}
+import lime.text.Font;
+import lime.text.TextLayout;
+import lime.utils.Float32Array;
+import lime.utils.GLUtils;
+import lime.utils.UInt8Array;
 
 
 class Main extends Application {
 	
 	
+	private var imageUniform:GLUniformLocation;
 	private var matrixUniform:GLUniformLocation;
 	private var program:GLProgram;
+	private var textFields = new Array<TextRender> ();
 	private var textureAttribute:Int;
 	private var vertexAttribute:Int;
-	private var textFields:Array<TextField>;
 	
 	
 	public function new () {
 		
 		super ();
-		textFields = new Array<TextField> ();
 		
 	}
 	
 	
 	public override function init (context:RenderContext):Void {
 		
-		textFields.push(new TextField ("صِف خَلقَ خَودِ كَمِثلِ الشَمسِ إِذ بَزَغَت — يَحظى الضَجيعُ بِها نَجلاءَ مِعطارِ", 16,
-			new TextEngine (RIGHT_TO_LEFT, ARABIC, "ar"),
-			Font.fromFile ("assets/amiri-regular.ttf"),
-			context, window.width, 80));
+		var font = Font.fromFile ("assets/amiri-regular.ttf");
+		var textLayout = new TextLayout ("صِف خَلقَ خَودِ كَمِثلِ الشَمسِ إِذ بَزَغَت — يَحظى الضَجيعُ بِها نَجلاءَ مِعطارِ", font, 16, RIGHT_TO_LEFT, ARABIC, "ar");
+		textFields.push (new TextRender (textLayout, window.width, 80));
 		
-		textFields.push(new TextField ("The quick brown fox jumps over the lazy dog.", 16,
-			new TextEngine (LEFT_TO_RIGHT, LATIN, "en"),
-			Font.fromFile ("assets/amiri-regular.ttf"),
-			context, 20, 20));
+		var textLayout = new TextLayout ("The quick brown fox jumps over the lazy dog.", font, 16);
+		textFields.push (new TextRender (textLayout, 20, 20));
 		
-		textFields.push(new TextField ("懶惰的姜貓", 32,
-			new TextEngine (TOP_TO_BOTTOM, HAN, "zh"),
-			Font.fromFile ("assets/fireflysung.ttf"),
-			context, 50, 170));
+		var font = Font.fromFile ("assets/fireflysung.ttf");
+		var textLayout = new TextLayout ("懶惰的姜貓", font, 32, TOP_TO_BOTTOM, HAN, "zh");
+		textFields.push (new TextRender (textLayout, 50, 170));
 		
 		switch (context) {
 			
@@ -262,7 +85,7 @@ class Main extends Application {
 				vertexAttribute = gl.getAttribLocation (program, "aPosition");
 				textureAttribute = gl.getAttribLocation (program, "aTexCoord");
 				matrixUniform = gl.getUniformLocation (program, "uMatrix");
-				var imageUniform = gl.getUniformLocation (program, "uImage0");
+				imageUniform = gl.getUniformLocation (program, "uImage0");
 				
 				gl.enableVertexAttribArray (vertexAttribute);
 				gl.enableVertexAttribArray (textureAttribute);
@@ -272,13 +95,20 @@ class Main extends Application {
 				gl.enable (gl.TEXTURE_2D);
 				#end
 				
-				gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-				gl.enable(gl.BLEND);
+				gl.blendFunc (gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+				gl.enable (gl.BLEND);
 				
 				gl.viewport (0, 0, window.width, window.height);
 				
 			default:
 				
+			
+		}
+		
+		for (textField in textFields) {
+			
+			textField.init (context);
+			
 		}
 		
 	}
@@ -301,14 +131,169 @@ class Main extends Application {
 				var matrix = Matrix4.createOrtho (0, window.width, window.height, 0, -10, 10);
 				gl.uniformMatrix4fv (matrixUniform, false, matrix);
 				
-				for (i in 0...textFields.length) {
+				for (textField in textFields) {
 					
-					textFields[i].render (context, vertexAttribute, textureAttribute);
+					textField.render (context, vertexAttribute, textureAttribute);
 					
 				}
 				
 			default:
 				
+		}
+		
+	}
+	
+	
+}
+
+
+class TextRender {
+	
+	
+	private var vertexBuffer:GLBuffer;
+	private var images:Map<Int, Image>;
+	private var indexBuffer:GLBuffer;
+	private var numTriangles:Int;
+	private var textLayout:TextLayout;
+	private var texture:GLTexture;
+	private var x:Float;
+	private var y:Float;
+	
+	
+	public function new (textLayout:TextLayout, x:Float = 0, y:Float = 0) {
+		
+		this.textLayout = textLayout;
+		this.x = x;
+		this.y = y;
+		
+		images = textLayout.font.renderGlyphs (textLayout.glyphs, textLayout.size);
+		
+	}
+	
+	
+	public function init (context:RenderContext) {
+		
+		switch (context) {
+			
+			case OPENGL (gl):
+				
+				var vertices = new Array<Float> ();
+				var indices = new Array<Int> ();
+				var left, top, right, bottom;
+				
+				if (textLayout.direction == RIGHT_TO_LEFT) {
+					
+					var width = 0.0;
+					
+					for (position in textLayout.positions) {
+						
+						width += position.advance.x;
+						
+					}
+					
+					x -= width;
+					
+				}
+				
+				var buffer = null;
+				
+				for (position in textLayout.positions) {
+					
+					if (images.exists (position.glyph)) {
+						
+						var image = images.get (position.glyph);
+						
+						buffer = image.buffer;
+						
+						left = image.offsetX / buffer.width;
+						top = image.offsetY / buffer.height;
+						right = left + image.width / buffer.width;
+						bottom = top + image.height / buffer.height;
+						
+						var pointLeft = x + position.offset.x + image.x;
+						var pointTop = y + position.offset.y - image.y;
+						var pointRight = pointLeft + image.width;
+						var pointBottom = pointTop + image.height;
+						
+						vertices.push (pointRight);
+						vertices.push (pointBottom);
+						vertices.push (right);
+						vertices.push (bottom);
+						
+						vertices.push (pointLeft);
+						vertices.push (pointBottom);
+						vertices.push (left);
+						vertices.push (bottom);
+						
+						vertices.push (pointRight);
+						vertices.push (pointTop);
+						vertices.push (right);
+						vertices.push (top);
+						
+						vertices.push (pointLeft);
+						vertices.push (pointTop);
+						vertices.push (left);
+						vertices.push (top);
+						
+						var i = Std.int (indices.length / 6) * 4;
+						indices.push (i);
+						indices.push (i + 1);
+						indices.push (i + 2);
+						indices.push (i + 1);
+						indices.push (i + 2);
+						indices.push (i + 3);
+						
+					}
+					
+					x += position.advance.x;
+					y -= position.advance.y; // flip because of y-axis direction
+					
+				}
+				
+				numTriangles = indices.length;
+				
+				vertexBuffer = gl.createBuffer ();
+				gl.bindBuffer (gl.ARRAY_BUFFER, vertexBuffer);
+				gl.bufferData (gl.ARRAY_BUFFER, new Float32Array (vertices), gl.STATIC_DRAW);
+				
+				indexBuffer = gl.createBuffer ();
+				gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+				gl.bufferData (gl.ELEMENT_ARRAY_BUFFER, new UInt8Array (indices), gl.STATIC_DRAW);
+				
+				var format = (buffer.bitsPerPixel == 1 ? gl.ALPHA : gl.RGBA);
+				texture = gl.createTexture ();
+				gl.bindTexture (gl.TEXTURE_2D, texture);
+				gl.texImage2D (gl.TEXTURE_2D, 0, format, buffer.width, buffer.height, 0, format, gl.UNSIGNED_BYTE, buffer.data);
+				gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+				gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+				
+			default:
+				
+			
+		}
+		
+	}
+	
+	
+	public function render (context:RenderContext, vertexAttribute:Int, textureAttribute:Int) {
+		
+		switch (context) {
+			
+			case OPENGL (gl):
+				
+				gl.activeTexture (gl.TEXTURE0);
+				gl.bindTexture (gl.TEXTURE_2D, texture);
+				
+				gl.bindBuffer (gl.ARRAY_BUFFER, vertexBuffer);
+				gl.vertexAttribPointer (vertexAttribute, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 0);
+				gl.vertexAttribPointer (textureAttribute, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+				
+				gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+				gl.drawElements (gl.TRIANGLES, numTriangles, gl.UNSIGNED_BYTE, 0);
+				
+			default:
+				
+			
 		}
 		
 	}

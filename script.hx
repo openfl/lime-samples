c2064e1087f201ce61c017a1d7dab1a3ca08323c
Joshua Granick <jgranick@users.noreply.github.com>
2018-08-08 10:24:03 -0700

    Update script

--- script.hx
+++ script.hx
@@ -1,125 +1,182 @@
+package;
+
+
 import hxp.*;
 import sys.FileSystem;
 
+
 class Script extends hxp.Script {
+	
+	
+	private var samples:Array<String>;
+	
 	
 	public function new () {
 		
 		super ();
 		
-		var paths = [];
-		getPaths ("demos", paths);
-		getPaths ("features/app", paths);
-		getPaths ("features/graphics", paths);
-		getPaths ("features/input", paths);
-		getPaths ("features/media", paths);
-		getPaths ("features/text", paths);
+		samples = [];
+		findSamples ("demos", samples);
+		findSamples ("features/app", samples);
+		findSamples ("features/graphics", samples);
+		findSamples ("features/input", samples);
+		findSamples ("features/media", samples);
+		findSamples ("features/text", samples);
 		
-		if (command == "list") {
+		switch (command) {
 			
-			for (path in paths) {
-				Log.println (path);
-			}
+			case "list":
+				
+				listSamples ();
 			
-		} else if (command == "test") {
+			case "build", "run", "test", "update", "clean", "display":
+				
+				execSamples ();
 			
-			if (commandArgs.length > 0) {
-				var sampleName = commandArgs.shift ();
-				var match = false;
-				for (path in paths) {
-					if (path.split ("/").pop () == sampleName) {
-						paths = [ path ];
-						match = true;
-						break;
-					}
-				}
-				if (!match) {
-					paths = [];
-					if (sampleName == "features") {
-						getPaths ("features/app", paths);
-						getPaths ("features/graphics", paths);
-						getPaths ("features/input", paths);
-						getPaths ("features/media", paths);
-						getPaths ("features/text", paths);
-					} else {
-						getPaths (sampleName, paths);
-					}
-				}
-			}
-			
-			var targets;
-			if (commandArgs.length > 0) {
-				targets = commandArgs;
-			} else {
-				var hostPlatform = switch (System.hostPlatform) {
-					case WINDOWS: "windows";
-					case MAC: "mac";
-					case LINUX: "linux";
-					default: "";
-				}
-				if (System.hostPlatform != MAC) {
-					targets = [ "neko", "neko -Dcairo", "flash", hostPlatform, "electron", "electron -Dcanvas", "electron -Ddom" ];
-				} else {
-					targets = [ "neko", "neko -Dcairo", /*"flash", hostPlatform,*/ "electron", "electron -Dcanvas", "electron -Ddom" ];
-				}
-			}
-			
-			for (path in paths) {
-				var sampleName = Path.standardize (path).split ("/").pop ();
-				for (target in targets) {
-					Log.info (Log.accentColor + "Running Sample: " + sampleName + " [" + target + "]" + Log.resetColor);
-					if (FileSystem.exists (Path.combine (path, "script.hx"))) {
-						if (target == "electron") continue; // TODO
-						var args = [ "test", Path.combine (path, "script.hx") ].concat (target.split (" "));
-						for (flag in flags.keys ()) {
-							args.push ("-" + flag);
-						}
-						for (define in defines.keys ()) {
-							args.push ("-D");
-							if (defines.get (define) != "") {
-								args.push (define + "=" + defines.get (define));
-							} else {
-								args.push (define);
-							}
-						}
-						System.runCommand ("", "hxp", args);
-					} else {
-						var args = [ "test", path, ].concat (target.split (" "));
-						for (flag in flags.keys ()) {
-							args.push ("-" + flag);
-						}
-						for (define in defines.keys ()) {
-							args.push ("-D");
-							if (defines.get (define) != "") {
-								args.push (define + "=" + defines.get (define));
-							} else {
-								args.push (define);
-							}
-						}
-						if (target == "flash") args.push ("-notrace");
-						System.runCommand ("", "lime", args);
-					}
-				}
-			}
-			
-		} else {
-			
-			Log.error ("Unknown command");
+			default:
+				
+				Log.error ("Unkown command \"" + command + "\"");
 			
 		}
 		
 	}
 	
-	private function getPaths (directory:String, paths:Array<String>):Void {
+	
+	private function execSamples ():Void {
 		
-		directory = Path.combine (Sys.getCwd (), directory);
-		for (path in FileSystem.readDirectory (directory)) {
-			path = Path.combine (directory, path);
-			if (FileSystem.isDirectory (path)) {
-				paths.push (Path.standardize (path));
+		var paths = [];
+		
+		if (commandArgs.length > 0) {
+			
+			var sampleName = commandArgs.shift ();
+			
+			for (sample in samples) {
+				if (sample.split ("/").pop () == sampleName) {
+					paths.push (sample);
+				}
 			}
+			
+			if (paths.length == 0) {
+				for (sample in samples) {
+					if (StringTools.startsWith (sample, sampleName)) {
+						paths.push (sample);
+					}
+				}
+			}
+			
+			if (paths.length == 0) {
+				Log.error ("Could not find sample name \"" + sampleName + "\"");
+			}
+			
+		} else {
+			
+			paths = paths.concat (samples);
+			
+		}
+		
+		var targets = null;
+		
+		if (commandArgs.length > 0) {
+			
+			targets = commandArgs;
+			
+		} else {
+			
+			var hostPlatform = switch (System.hostPlatform) {
+				case WINDOWS: "windows";
+				case MAC: "mac";
+				case LINUX: "linux";
+				default: "";
+			}
+			
+			targets = [];
+			if (!flags.exists ("noneko")) targets.push ("neko");
+			if (!flags.exists ("noneko") && !flags.exists ("nocairo")) targets.push ("neko -Dcairo");
+			if (!flags.exists ("noelectron")) targets.push ("electron");
+			if (!flags.exists ("noelectron") && !flags.exists ("nocanvas")) targets.push ("electron -Dcanvas");
+			if (!flags.exists ("noelectron") && !flags.exists ("nodom")) targets.push ("electron -dom");
+			if (!flags.exists ("nocpp")) targets.push (hostPlatform);
+			if (!flags.exists ("noflash")) targets.push ("flash");
+			
+		}
+		
+		for (path in paths) {
+			
+			var sampleName = path.split ("/").pop ();
+			
+			for (target in targets) {
+				
+				Log.info (Log.accentColor + "Running Sample: " + sampleName + " [" + target + "]" + Log.resetColor);
+				
+				var script = "lime";
+				var args = [ command ];
+				
+				if (FileSystem.exists (Path.combine (path, "script.hx"))) {
+					
+					script = "hxp";
+					args.push (Path.combine (path, "script.hx"));
+					
+				} else {
+					
+					args.push (path);
+					
+				}
+				
+				args = args.concat (target.split (" "));
+				
+				for (flag in flags.keys ()) {
+					args.push ("-" + flag);
+				}
+				
+				for (define in defines.keys ()) {
+					args.push ("-D");
+					if (defines.get (define) != "") {
+						args.push (define + "=" + defines.get (define));
+					} else {
+						args.push (define);
+					}
+				}
+				
+				if (target == "flash" && targets.length > 0) {
+					args.push ("-notrace");
+				}
+				
+				System.runCommand ("", script, args);
+				
+			}
+			
 		}
 		
 	}
+	
+	
+	private function findSamples (path:String, list:Array<String>):Void {
+		
+		for (fileName in FileSystem.readDirectory (path)) {
+			
+			var filePath = Path.combine (path, fileName);
+			
+			if (FileSystem.isDirectory (filePath)) {
+				
+				list.push (Path.standardize (filePath));
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	private function listSamples ():Void {
+		
+		for (sample in samples) {
+			
+			var sampleName = sample.split ("/").pop ();
+			Log.println (Log.accentColor + sampleName + Log.resetColor + " (" + sample + ")");
+			
+		}
+		
+	}
+	
 	
 }
